98f7d6e339ebbb361d8413d6132d7b4a211ee9ff
Keanu Anthony Monterde <keanuanthony.monterde.21@usjr.edu.ph>
2023-09-28 14:49:37 +0800

    Added: Triangle - Compute: Volume ; Perimeter; Area
    To Add: Units beside result

--- ShapeCalculator/Model/TriangleModel.cs
+++ ShapeCalculator/Model/TriangleModel.cs
@@ -11,10 +11,50 @@ namespace ShapeCalculator.Model
     {
         private double _base;
         private double _height;
-        private double _area;
         private double _volume;
-        private double _perimeter;
+        private double _side1;
+        private double _side2;
+        private double _side3;
+        private double _radius;
 
+
+        public double Radius
+        {
+            get => _radius;
+            set
+            {
+                _radius = value;
+                RaisePropertyChanged(nameof(Radius));
+            }
+        }
+
+
+        public double Side1
+        {
+            get => _side1;
+            set{
+                _side1 = value;
+                RaisePropertyChanged(nameof(Side1));
+            }
+        }
+        public double Side2
+        {
+            get => _side2;
+            set
+            {
+                _side2 = value;
+                RaisePropertyChanged(nameof(Side2));
+            }
+        }
+        public double Side3
+        {
+            get => _side3;
+            set
+            {
+                _side3 = value;
+                RaisePropertyChanged(nameof(Side3));
+            }
+        }
         public double Base
         {
             get => _base;
@@ -42,14 +82,19 @@ namespace ShapeCalculator.Model
                 RaisePropertyChanged(nameof(Volume));
             }
         }
-        private void CalculatePerimeter()
+        public void CalculatePerimeter()
         {
-            throw new NotImplementedException();
+            Perimeter = Side1 + Side2 + Side3;
         }
 
         public void CalculateArea()
         {
             Area = Base * Height / 2;
+        }
+
+        public void CalculateVolume()
+        {
+            Volume = Math.Round((Math.PI * Math.Pow(Radius, 2) * Height) / 3,4);
         }
 
         public TriangleModel() {        }
--- ShapeCalculator/ViewModel/TriangleView.xaml.cs
+++ ShapeCalculator/ViewModel/TriangleView.xaml.cs
@@ -11,13 +11,62 @@ public partial class TriangleView : ContentPage
 
         InitializeComponent();
 		this.BindingContext = activeTriangle = new TriangleModel();
+		activeUnitRow_0.SelectedIndex = 0;
+		activeUnitRow_1.SelectedIndex = 0;
+		activeUnitRow_2.SelectedIndex = 0;
+		
 	}
+	
 
 	public void OnClickBtnCalculate(object sender, EventArgs e)
 	{
-        activeTriangle.Base = double.Parse(txtBase.Text);
-        activeTriangle.Height = double.Parse(txtHeight.Text);
-		activeTriangle.CalculateArea();
+        
+        int computationCalc = Int32.Parse(((Button)sender).AutomationId);
+		try
+		{
+            if (computationCalc == 0)
+            {
+                activeTriangle.Base = double.Parse(txtBase.Text);
+                activeTriangle.Height = double.Parse(txtHeight.Text);
+                activeTriangle.CalculateArea();
+            }
+            else if (computationCalc == 1)
+            {
+                activeTriangle.Side1 = double.Parse(txtSide1.Text);
+                activeTriangle.Side2 = double.Parse(txtSide2.Text);
+                activeTriangle.Side3 = double.Parse(txtSide3.Text);
+                activeTriangle.CalculatePerimeter();
+            }
+            else if (computationCalc == 2)
+            {
+                activeTriangle.Radius = double.Parse(txtRadius.Text);
+                activeTriangle.Height = double.Parse(txtHeight2.Text);
+                activeTriangle.CalculateVolume();
+            }
+        }catch(Exception err)
+        {
+            Console.WriteLine(err);
+            DisplayAlert("Error", "Invalid Input", "Ok");
+            if (computationCalc == 0)
+            {
+                activeTriangle.Base = 0;
+                activeTriangle.Height = 0;
+                activeTriangle.Area = 0;
+            }
+            else if (computationCalc == 1)
+            {
+                activeTriangle.Side1 = 0;
+                activeTriangle.Side2 = 0;
+                activeTriangle.Side3 = 0;
+                activeTriangle.Perimeter = 0;
+            }
+            else if (computationCalc == 2)
+            {
+                activeTriangle.Radius = 0;
+                activeTriangle.Height = 0;
+                activeTriangle.Volume = 0;
+            }
+        }
     }
 
 }
